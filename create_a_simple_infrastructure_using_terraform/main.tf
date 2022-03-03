0cd47f5abbba6960d33e8dcde92e9adc6a55859e
vaibhavkatoch <vaibhavkatoch04@gmail.com>
2022-03-03 11:28:02 +0530

    Building a simple infrastructure using module

---
+++ create_a_simple_infrastructure_using_terraform/main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+#The module will create a new vpc and all the requires resources 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Name = "Terraform"
+    Environment = "dev"
+  }
+}
+
+#launching an EC2 instance
+
+resource "aws_instance" "Ubuntu" {
+    ami = "ami-04505e74c0741db8d"
+    instance_type = "t2.micro"
+    key_name = "New-acc"
+    subnet_id = module.vpc.public_subnets[0]
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    tags = {
+        "Name" = "module-EC2"
+    }
+}
+#creating a new-security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
